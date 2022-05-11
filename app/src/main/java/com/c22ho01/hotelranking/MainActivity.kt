99c987227418d902d69440fa2f49fecc5e26e731
Kiki Abdullah <kikyabdullah@live.com>
2022-05-11 19:55:55 +0700

    initial project

---
+++ app/src/main/java/com/c22ho01/hotelranking/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.c22ho01.hotelranking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
