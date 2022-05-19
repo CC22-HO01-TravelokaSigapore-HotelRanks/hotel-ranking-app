6cd8d196158781c99c1e9c061a4d996ce0fe4086
Catyousha <jakaasha@gmail.com>
2022-05-19 11:16:01 +0700

    feat: add login & register response

--- app/src/main/java/com/c22ho01/hotelranking/data/remote/retrofit/AuthService.kt
+++ app/src/main/java/com/c22ho01/hotelranking/data/remote/retrofit/AuthService.kt
@@ -1,4 +1,24 @@
 package com.c22ho01.hotelranking.data.remote.retrofit
 
+import com.c22ho01.hotelranking.data.remote.response.auth.LoginResponse
+import com.c22ho01.hotelranking.data.remote.response.auth.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface AuthService {
+    @POST("/user/login")
+    @FormUrlEncoded
+    suspend fun login(
+        @Field("userName") userName: String,
+        @Field("password") password: String
+    ): LoginResponse
+
+    @POST("/user/register")
+    @FormUrlEncoded
+    suspend fun register(
+        @Field("userName") userName: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
 }
