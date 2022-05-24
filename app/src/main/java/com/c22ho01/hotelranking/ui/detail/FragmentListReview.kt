b67e3e0e55d38c1e1fe0d53846fee17a1031951b
Kiki Abdullah <kikyabdullah@live.com>
2022-05-24 20:03:06 +0700

    style: reformat code and remove some duplicates strings.xml

--- app/src/main/java/com/c22ho01/hotelranking/ui/detail/FragmentListReview.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/detail/FragmentListReview.kt
@@ -1,10 +1,10 @@
 package com.c22ho01.hotelranking.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.c22ho01.hotelranking.R
 
 // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/c22ho01/hotelranking/ui/detail/PreviewMapsFragment.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/detail/PreviewMapsFragment.kt
@@ -1,15 +1,12 @@
 package com.c22ho01.hotelranking.ui.detail
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.c22ho01.hotelranking.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
