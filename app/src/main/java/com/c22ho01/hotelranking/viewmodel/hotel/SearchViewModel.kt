3bc223afa1a601e329da21bdcad2f261557fc34e
Kiki Abdullah <kikyabdullah@live.com>
2022-05-25 15:34:49 +0700

    feat: add pagination for SearchActivity.kt

---
+++ app/src/main/java/com/c22ho01/hotelranking/viewmodel/hotel/SearchViewModel.kt
@@ -0,0 +1,16 @@
+package com.c22ho01.hotelranking.viewmodel.hotel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.c22ho01.hotelranking.data.remote.response.hotel.HotelData
+import com.c22ho01.hotelranking.data.repository.HotelRepository
+
+class SearchViewModel(private val hotelRepository: HotelRepository) : ViewModel() {
+
+    fun searchHotel(keyword: String): LiveData<PagingData<HotelData>> {
+        return hotelRepository.searchHotel(keyword).cachedIn(viewModelScope)
+    }
+}
