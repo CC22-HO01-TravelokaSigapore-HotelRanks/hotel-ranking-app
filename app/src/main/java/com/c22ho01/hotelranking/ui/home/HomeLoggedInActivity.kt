b3b06c880f6d4370b3f6a89ce58bff88c691b432
Kiki Abdullah <kikyabdullah@live.com>
2022-06-04 22:23:17 +0700

    fix: fix inconsistent color and UI

--- app/src/main/java/com/c22ho01/hotelranking/ui/home/HomeLoggedInActivity.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/home/HomeLoggedInActivity.kt
@@ -1,5 +1,6 @@
 package com.c22ho01.hotelranking.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -23,5 +24,9 @@ class HomeLoggedInActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_home_logged_in) as NavHostFragment
         navView.setupWithNavController(navHostFragment.navController)
+
+        binding.searchBar.setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
+        }
     }
 }
