e46aed089c3a17573f9c6b4b8e5c5a23558e479b
Kiki Abdullah <kikyabdullah@live.com>
2022-06-05 11:02:01 +0700

    feat: beautify some UI's

--- app/src/main/java/com/c22ho01/hotelranking/ui/home/HomeGuestActivity.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/home/HomeGuestActivity.kt
@@ -2,25 +2,43 @@ package com.c22ho01.hotelranking.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.c22ho01.hotelranking.R
 import com.c22ho01.hotelranking.databinding.ActivityHomeGuestBinding
+import com.c22ho01.hotelranking.viewmodel.ViewModelFactory
+import com.c22ho01.hotelranking.viewmodel.utils.TokenViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HomeGuestActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeGuestBinding
+    private lateinit var factory: ViewModelFactory
+    private val tokenViewModel by viewModels<TokenViewModel> { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        factory = ViewModelFactory.getInstance(this)
+        lifecycleScope.launch {
+            tokenViewModel.getToken().collect {
+                if (it != null) {
+                    startActivity(
+                        Intent(
+                            this@HomeGuestActivity, HomeLoggedInActivity::class.java
+                        )
+                    )
+                }
+            }
+        }
         super.onCreate(savedInstanceState)
-
         binding = ActivityHomeGuestBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_activity_home_guest) as NavHostFragment
         navView.setupWithNavController(navHostFragment.navController)
