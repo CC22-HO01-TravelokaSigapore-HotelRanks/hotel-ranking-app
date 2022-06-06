28cb0c96c0e736eb2bb1162f3668235756487544
Catyousha <jakaasha@gmail.com>
2022-06-06 16:28:55 +0700

    feat: implement for you api

--- app/src/main/java/com/c22ho01/hotelranking/ui/foryou/ForYouActivity.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/foryou/ForYouActivity.kt
@@ -3,25 +3,32 @@ package com.c22ho01.hotelranking.ui.foryou
 import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.c22ho01.hotelranking.adapter.ListForYouAdapter
+import com.c22ho01.hotelranking.data.Result
 import com.c22ho01.hotelranking.data.remote.response.hotel.UserLocation
 import com.c22ho01.hotelranking.databinding.ActivityForYouBinding
 import com.c22ho01.hotelranking.viewmodel.ViewModelFactory
 import com.c22ho01.hotelranking.viewmodel.hotel.ForYouViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class ForYouActivity : AppCompatActivity() {
     private var _binding: ActivityForYouBinding? = null
     private val binding get() = _binding
-    private var userLocation: UserLocation? = null
+    private lateinit var userLocation: UserLocation
     private lateinit var factory: ViewModelFactory
-    private lateinit var forYouViewModel: ForYouViewModel
+    private val forYouViewModel: ForYouViewModel by viewModels { factory }
+
+    private lateinit var forYouAdapter: ListForYouAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityForYouBinding.inflate(layoutInflater)
         factory = ViewModelFactory.getInstance(this)
-        userLocation = intent.getParcelableExtra(USER_LOCATION_EXTRA)
-
+        userLocation = intent.getParcelableExtra(USER_LOCATION_EXTRA) ?: UserLocation()
+        forYouAdapter = ListForYouAdapter()
 
         binding?.appbarMtForYou?.setNavigationOnClickListener {
             onBackPressed()
@@ -32,7 +39,37 @@ class ForYouActivity : AppCompatActivity() {
     }
 
     private fun loadForYouHotels() {
+        forYouViewModel.getForYouHotels(userLocation).run {
+            if (this.hasObservers()) {
+                this.removeObservers(this@ForYouActivity)
+            }
+            this.observe(this@ForYouActivity) { result ->
+                when (result) {
+                    is Result.Loading -> {
+                        showLoading(true)
+                    }
+                    is Result.Success -> {
+                        showLoading(false)
+                        binding?.rvForYou?.run {
+                            forYouAdapter.submitList(result.data.data)
+                            adapter = forYouAdapter
+                            layoutManager = LinearLayoutManager(this@ForYouActivity)
+                        }
+                    }
+                    is Result.Error -> {
+                        showLoading(false)
+                        binding?.let { activityBinding ->
+                            Snackbar.make(
+                                activityBinding.root,
+                                result.error,
+                                Snackbar.LENGTH_LONG,
+                            ).show()
+                        }
+                    }
+                }
 
+            }
+        }
     }
 
 
