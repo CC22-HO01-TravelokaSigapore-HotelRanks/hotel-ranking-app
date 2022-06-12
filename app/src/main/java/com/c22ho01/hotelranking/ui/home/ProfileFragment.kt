4057b522674e60fbc010f20a86bfbc2c6921a50a
Catyousha <jakaasha@gmail.com>
2022-06-12 18:46:54 +0700

    fix: profile load when no current profile found

--- app/src/main/java/com/c22ho01/hotelranking/ui/home/ProfileFragment.kt
+++ app/src/main/java/com/c22ho01/hotelranking/ui/home/ProfileFragment.kt
@@ -42,7 +42,7 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (profileViewModel.getCurrentProfile().value?.id != null) {
+        if (profileViewModel.getCurrentProfile().value?.id == null) {
             loadProfile()
         } else {
             initializeProfileData()
